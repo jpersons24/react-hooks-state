dcc02f9fbc17e01e5c53eb2da5e42764b98d1d35
Jakob Persons <jakobep24@gmail.com>
2021-02-02 08:22:01 -0500

    walk through

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 
 function Counter() {
+  // using array destructing to access useState array elements
   const [count, setCount] = useState(0);
 
+  // function increment() {
+  //   setCount(count + 1);
+  // }
+
+  // return <button onClick={increment}>I have been clicked {count} times</button>;
+
   function increment() {
-    setCount(count + 1);
+    // callback version of setCount --> offsets asychronous nature of setCount
+    // anytime we need to set state based on current value of state --> use callback syntax
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <div onClick={increment}>{count}</div>;
+
 }
 
 export default Counter;
